e2385f07c39b74d158844bd5c85d9139b5dab0be
Alan <avallve@opentrends.net>
2018-01-28 20:52:28 +0100

    #10 saca las imagenes, falta descargarlas

--- src/main/java/artwork/service/gendata/task.java
+++ src/main/java/artwork/service/gendata/task.java
@@ -1,11 +1,14 @@
 package artwork.service.gendata;
 
+import artwork.service.gendata.entitiesPhoto.LinksRetrofit;
 import com.google.gson.Gson;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class task {
 
@@ -13,29 +16,42 @@ public class task {
 
         InputStream in = null;
         try {
-            String appId = "01907a24370e291c3494c6d185612066075a3cc81852ea977fe6949da5daa234";
-            String urlStr = "https://api.unsplash.com/photos/random?client_id="+appId;
 
-            URL url = new URL(urlStr);
-            URLConnection conn = url.openConnection();
-            conn.setConnectTimeout(2000);
-            conn.setReadTimeout(1000);
-            int length = conn.getContentLength();
+            List<String> links = new ArrayList<>();
+            int i = 0;
+            while(i++ < 10) {
+
+                String appId = "01907a24370e291c3494c6d185612066075a3cc81852ea977fe6949da5daa234";
+                String urlStr = "https://api.unsplash.com/photos/random?client_id=" + appId;
+
+                URL url = new URL(urlStr);
+                URLConnection conn = url.openConnection();
+                conn.setConnectTimeout(2000);
+                conn.setReadTimeout(1000);
+                int length = conn.getContentLength();
 
 
-            in = url.openStream();
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int total = 0, nRead;
-            while ((nRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, nRead);
-                total += nRead;
+                in = url.openStream();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int total = 0, nRead;
+                while ((nRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, nRead);
+                    total += nRead;
+                }
+                String json = new String(out.toByteArray());
+
+
+                Gson gson = new Gson();
+                LinksRetrofit list = gson.fromJson(json, LinksRetrofit.class);
+
+                /*System.out.println(list.getLinks().getDownload());
+                System.out.println(list.getLinks().getDownloadLocation());*/
+
+                links.add(list.getLinks().getDownload());
             }
-            String json = new String(out.toByteArray());
 
-
-            Gson gson = new Gson();
-            //ReceivedGame list = gson.fromJson(json, ReceivedGame.class);
+            links.forEach(System.out::println);
         }
         catch (Exception e) {
             System.out.println(e);
